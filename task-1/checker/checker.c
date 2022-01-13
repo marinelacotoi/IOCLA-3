c624366c74b5e7fcbcb51a8f0c3b8b28f965c336
Marinela Cotoi <marinelacotoi@yahoo.com>
2022-01-13 15:58:27 +0200

    Task 1&2 done

--- task-1/checker/checker.c
+++ task-1/checker/checker.c
@@ -2,31 +2,35 @@
 
 // DO NOT MODIFY THIS FILE
 
-struct node {
+struct node
+{
     int val;
-    struct node* next;
+    struct node *next;
 };
 
-struct node* sort(int n, struct node* node);
+struct node *sort(int n, struct node *node);
 
-int main() {
+int main()
+{
     int n;
     scanf("%d", &n);
 
     struct node nodes[n];
 
-    for (int i = 0; i < n; i++) {
+    for (int i = 0; i < n; i++)
+    {
         scanf("%d", &(nodes[i].val));
         nodes[i].next = NULL;
     }
 
-    struct node* head = sort(n, nodes);
-    
-    while (head != NULL) {
+    struct node *head = sort(n, nodes);
+
+    while (head != NULL)
+    {
         printf("%d ", head->val);
         head = head->next;
     }
-    
+
     printf("\n");
     return 0;
 }
